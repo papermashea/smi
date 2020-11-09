a8517825452a5cb425b08d1aa166e07005846aa0
papermashea <molloy.shea@gmail.com>
2020-11-09 16:41:27 -0500

    adding menu js

--- js/global.js
+++ js/global.js
@@ -1,10 +1,10 @@
-// function toggle_visibility(id) {
-//        var e = document.getElementById(id);
-//        if(e.style.display == 'block')
-//           e.style.display = 'none';
-//        else
-//           e.style.display = 'block';
-//     }
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
 
 function menu() {
 	var pages = document.getElementByClassName("menu")
@@ -13,15 +13,5 @@ function menu() {
 	else
 		pages.style.display = 'block'
 }
-      // <ul class="menu-list" id="items">
-      //   <li><a href="../index.html" class="menu-item">home</a></li>
-      //   <li><a href="eddi.html" class="menu-item">eddi</a></li>
-      //   <li><a href="gdi.html" class="menu-item">gdi</a></li>
-      //   <li><a href="unglitch.html" class="menu-item">unglitch</a></li>
-      //   <li><a href="littlecinema.html" class="menu-item">little cinema</a></li>        
-      //   <li><a href="dol.html" class="menu-item">dol</a></li>        
-      //   <li><a href="tradecraft.html" class="menu-item">tradecraft</a></li>
-      //   <li><a href="templatestools.html" class="menu-item">templates repo</a></li>
-      //   <li><a href="creativeCoding.html" class="menu-item">creative coding</a></li>
-      //   <li><a href="other.html" class="menu-item">other</a></li>
-      // </ul>
+
+

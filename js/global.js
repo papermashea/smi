beb265c4b18346c35cbdbb5af3cee8c66fd691ff
papermashea <molloy.shea@gmail.com>
2020-11-09 12:48:15 -0500

    updating parsons coursework

--- js/global.js
+++ js/global.js
@@ -1,7 +1,27 @@
-function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
-    }
+// function toggle_visibility(id) {
+//        var e = document.getElementById(id);
+//        if(e.style.display == 'block')
+//           e.style.display = 'none';
+//        else
+//           e.style.display = 'block';
+//     }
+
+function menu() {
+	var pages = document.getElementByClassName("menu")
+	if(pages.style.display == 'block')
+		pages.style.display = 'none';
+	else
+		pages.style.display = 'block'
+}
+      // <ul class="menu-list" id="items">
+      //   <li><a href="../index.html" class="menu-item">home</a></li>
+      //   <li><a href="eddi.html" class="menu-item">eddi</a></li>
+      //   <li><a href="gdi.html" class="menu-item">gdi</a></li>
+      //   <li><a href="unglitch.html" class="menu-item">unglitch</a></li>
+      //   <li><a href="littlecinema.html" class="menu-item">little cinema</a></li>        
+      //   <li><a href="dol.html" class="menu-item">dol</a></li>        
+      //   <li><a href="tradecraft.html" class="menu-item">tradecraft</a></li>
+      //   <li><a href="templatestools.html" class="menu-item">templates repo</a></li>
+      //   <li><a href="creativeCoding.html" class="menu-item">creative coding</a></li>
+      //   <li><a href="other.html" class="menu-item">other</a></li>
+      // </ul>
